2840d64c03966dcc3d6b25ee430d24a987d5fe56
Yoann Gern <yoann@yoanngern.ch>
2017-11-15 12:47:31 +0100

    update RSS feed

--- wp-content/themes/iahm_2016/feed-mg.php
+++ wp-content/themes/iahm_2016/feed-mg.php
@@ -57,7 +57,7 @@ echo '<?xml version="1.0" encoding="' . get_option( 'blog_charset' ) . '"?' . '>
 		?>
 
 
-        <item>
+        <entry>
             <id><?php the_guid(); ?></id>
             <title type="html"><![CDATA[<?php the_title(); ?>]]></title>
             <author>
@@ -70,7 +70,7 @@ echo '<?xml version="1.0" encoding="' . get_option( 'blog_charset' ) . '"?' . '>
                     type="image/jpg"/>
             <summary type="html"><![CDATA[<?php echo "<p>test</p>" ?>]]></summary>
             <content type="html"><![CDATA[<?php echo "<p>test</p>" ?>]]></content>
-        </item>
+        </entry>
 
 	<?php endforeach; ?>
     <!-- End loop -->
